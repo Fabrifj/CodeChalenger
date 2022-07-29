e4c6ef4cd63fdd38b4d265365389bdf8741eede3
Fabrifj <fabrifj@gmail.com>
2022-07-29 09:39:52 -0400

    Add components, services and routes

--- code-challenge/src/app/app.module.ts
+++ code-challenge/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './components/index/index.component';
+import { ResultsComponent } from './components/results/results.component';
+import { ResultListComponent } from './shared-components/result-list/result-list.component';
+import { ResultItemComponent } from './shared-components/result-item/result-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    ResultsComponent,
+    ResultListComponent,
+    ResultItemComponent
   ],
   imports: [
     BrowserModule,

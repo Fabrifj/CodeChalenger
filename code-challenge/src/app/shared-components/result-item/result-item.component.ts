1c4cf2670abee55670a5fadf0457f91b0ecb613b
Fabrifj <fabrifj@gmail.com>
2022-07-30 11:35:50 -0400

    finish sound and contact

--- code-challenge/src/app/shared-components/result-item/result-item.component.ts
+++ code-challenge/src/app/shared-components/result-item/result-item.component.ts
@@ -7,9 +7,19 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ResultItemComponent implements OnInit {
   @Input() result:any;
+  isplay=false
+  audio = new Audio();
   constructor() { }
 
   ngOnInit(): void {
-  }
+    this.audio.src = this.result.previewUrl;
+    this.audio.load();
 
+  }
+  playAudio(){
+    this.isplay ? this.audio.pause() :this.audio.play()
+    this.isplay = !this.isplay
+    console.log(this.isplay)
+
+  }
 }
